ab2153846c0a77e2fc09e8aeb56ca89923c9d9f7
Mai Cường Thọ <thomc@ntu.edu.vn>
2024-09-24 13:14:56 +0700

    Tín xong BMI

--- TinhBMI/src/thomc/BaiTap/BMICalculator.java
+++ TinhBMI/src/thomc/BaiTap/BMICalculator.java
@@ -17,9 +17,12 @@ public class BMICalculator {
 		System.out.print("Cân nặng (kg):");
 		double canNang = banPhim.nextDouble();
 		
+		// Tính chỉ số BMI
+		double bmi = canNang/ (chieuCao*chieuCao);
 		
-		
-		
+		// In thông tin ra cho người dùng
+		System.out.println("Bạn " + hoTen);
+		System.out.println("Có chỉ số BMI = " + bmi);
 
 	}
 
