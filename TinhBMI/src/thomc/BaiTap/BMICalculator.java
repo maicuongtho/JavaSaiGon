a92ac631d2ec328a52ffbed4b3f3de205bff953c
Mai Cường Thọ <thomc@ntu.edu.vn>
2024-09-24 13:10:26 +0700

    THêm được nhập dữ liệu

--- TinhBMI/src/thomc/BaiTap/BMICalculator.java
+++ TinhBMI/src/thomc/BaiTap/BMICalculator.java
@@ -1,9 +1,25 @@
 package thomc.BaiTap;
 
+import java.util.Scanner;
+
 public class BMICalculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner banPhim = new Scanner(System.in);
+		//Nhập Họ tên
+		System.out.print("Họ tên: ");
+		String hoTen= banPhim.nextLine();
+		// Nhập chiều cao 
+		System.out.print("Chiều cao (m):");
+		double chieuCao =  banPhim.nextDouble();
+		// Nhập cân nặng
+		System.out.print("Cân nặng (kg):");
+		double canNang = banPhim.nextDouble();
+		
+		
+		
+		
 
 	}
 
